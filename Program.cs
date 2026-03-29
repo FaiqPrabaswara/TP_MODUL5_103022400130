d81138347e7eaa52405e067a7abba3c72af822ec
Faiq Prabaswara <faiqprabaswara1101@gmail.com>
2026-03-29 22:25:12 +0700

    commit message

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TP_MODUL5_103022400130;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("103022400130");
+
+        data.PrintData();
+    }
+}
